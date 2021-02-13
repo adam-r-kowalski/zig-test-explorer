c3550e950df7f416aba307c0d670ae37a239a2ff
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-02-13 14:36:31 -0800

    better architecture for adaptor

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,62 +1,36 @@
 import * as vscode from 'vscode';
 import { TestAdapter, TestLoadStartedEvent, TestLoadFinishedEvent, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
-import { Log } from 'vscode-test-adapter-util';
-import { runFakeTests } from './fakeTests';
-import { loadZigTests } from './zigTests';
+import { loadZigTests, runZigTests, ProjectTests } from './zigTests';
 
-/**
- * This class is intended as a starting point for implementing a "real" TestAdapter.
- * The file `README.md` contains further instructions.
- */
-export class ExampleAdapter implements TestAdapter {
-
+export class ZigTestAdapter implements TestAdapter {
 	private disposables: { dispose(): void }[] = [];
-
 	private readonly testsEmitter = new vscode.EventEmitter<TestLoadStartedEvent | TestLoadFinishedEvent>();
 	private readonly testStatesEmitter = new vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>();
 	private readonly autorunEmitter = new vscode.EventEmitter<void>();
+	private projectTests?: ProjectTests = undefined;
 
 	get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> { return this.testsEmitter.event; }
 	get testStates(): vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent> { return this.testStatesEmitter.event; }
 	get autorun(): vscode.Event<void> | undefined { return this.autorunEmitter.event; }
 
-	constructor(
-		public readonly workspace: vscode.WorkspaceFolder,
-		private readonly log: Log
-	) {
-
-		this.log.info('Initializing example adapter');
-
+	constructor(public readonly workspace: vscode.WorkspaceFolder) {
 		this.disposables.push(this.testsEmitter);
 		this.disposables.push(this.testStatesEmitter);
 		this.disposables.push(this.autorunEmitter);
-
 	}
 
 	async load(): Promise<void> {
-
-		this.log.info('Loading example tests');
-
 		this.testsEmitter.fire(<TestLoadStartedEvent>{ type: 'started' });
-
-		// const loadedTests = await loadFakeTests();
-		const loadedTests = await loadZigTests(this.workspace);
-
-		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: loadedTests });
-
+		this.projectTests = await loadZigTests(this.workspace);
+		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: this.projectTests.testSuiteInfo });
 	}
 
 	async run(tests: string[]): Promise<void> {
-
-		this.log.info(`Running example tests ${JSON.stringify(tests)}`);
-
 		this.testStatesEmitter.fire(<TestRunStartedEvent>{ type: 'started', tests });
-
-		// in a "real" TestAdapter this would start a test run in a child process
-		await runFakeTests(tests, this.testStatesEmitter);
-
+		if (this.projectTests) {
+			await runZigTests(this.projectTests, tests, this.testStatesEmitter);
+		}
 		this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });
-
 	}
 
 	/*	implement this method if your TestAdapter supports debugging tests
--- src/zigTests.ts
+++ src/zigTests.ts
@@ -4,41 +4,6 @@ import * as fs from 'fs';
 import * as readline from 'readline';
 import { once } from 'events';
 
-const zigTestSuite: TestSuiteInfo = {
-    type: 'suite',
-    id: 'root',
-    label: 'Zig Test', // the label of the root node should be the name of the testing framework
-    children: [
-        {
-            type: 'suite',
-            id: 'nested',
-            label: 'Nested suite',
-            children: [
-                {
-                    type: 'test',
-                    id: 'test1',
-                    label: 'Test #1'
-                },
-                {
-                    type: 'test',
-                    id: 'test2',
-                    label: 'Test #2'
-                }
-            ]
-        },
-        {
-            type: 'test',
-            id: 'test3',
-            label: 'Test #3'
-        },
-        {
-            type: 'test',
-            id: 'test4',
-            label: 'Test #4'
-        }
-    ]
-};
-
 const readdir = async (path: fs.PathLike): Promise<string[]> =>
     new Promise((resolve, reject) =>
         fs.readdir(path, (err, content) =>
@@ -46,104 +11,126 @@ const readdir = async (path: fs.PathLike): Promise<string[]> =>
         )
     );
 
-const loadTests = async (path: string): Promise<TestInfo[]> => {
-    let children: TestInfo[] = [];
+enum Kind {
+    Test,
+    Suite,
+}
 
+interface Suite {
+    kinds: Kind[];
+    children: number[];
+}
+
+export interface ProjectTests {
+    testSuiteInfo: TestSuiteInfo;
+    mapping: { [id: string]: number };
+    kinds: Kind[];
+    indices: number[];
+    suites: Suite[];
+    filenames: string[];
+    tests: string[];
+}
+
+const regex = /test\s*"(?<label>[^"]*)"/;
+
+const loadTests = async (projectTests: ProjectTests, path: string, testInfos: TestInfo[], suite: Suite): Promise<void> => {
     const rl = readline.createInterface({
         input: fs.createReadStream(path),
         output: process.stdout,
         terminal: false
     });
 
-    const regex = /test\s*"(?<name>[^"]*)"/;
     let row = 0;
 
     rl.on('line', (line) => {
         const match = regex.exec(line);
         if (match) {
-            children.push({
+            const label = match.groups?.label ?? '';
+            const id = path + ' ' + label;
+            testInfos.push({
                 type: 'test',
-                id: path + '/' + line,
-                label: match.groups?.name ?? 'anonymous',
+                id: id,
+                label: label,
                 file: path,
                 line: row
             });
+            const test_id = projectTests.kinds.length;
+            projectTests.mapping[id] = test_id;
+            projectTests.kinds.push(Kind.Test);
+            projectTests.indices.push(projectTests.tests.length);
+            projectTests.filenames.push(path);
+            projectTests.tests.push(label);
+            suite.kinds.push(Kind.Test);
+            suite.children.push(test_id);
         }
         row += 1;
     });
 
     await once(rl, 'close');
-    return children;
 }
 
-const loadTestSuite = async (path: fs.PathLike): Promise<TestSuiteInfo[]> => {
-    let children: TestSuiteInfo[] = [];
+const loadTestSuite = async (projectTests: ProjectTests, path: string, children: (TestSuiteInfo | TestInfo)[]): Promise<void> => {
     const files = await readdir(path);
     for (const file of files) {
         if (file.endsWith('.zig')) {
             const filePath = path + '/' + file;
+            projectTests.mapping[filePath] = projectTests.kinds.length;
+            projectTests.kinds.push(Kind.Suite);
+            projectTests.indices.push(projectTests.suites.length);
+            let suite = {
+                kinds: [],
+                children: []
+            };
+            projectTests.suites.push(suite);
+            let testInfos: TestInfo[] = [];
+            await loadTests(projectTests, filePath, testInfos, suite);
             children.push({
                 type: 'suite',
                 id: filePath,
                 label: file,
-                children: await loadTests(filePath)
+                children: testInfos,
             });
         }
     }
-    return children;
 }
 
-export const loadZigTests = async (workspace: vscode.WorkspaceFolder): Promise<TestSuiteInfo> => ({
-    type: 'suite',
-    id: 'root',
-    label: 'Zig Test',
-    children: await loadTestSuite(workspace.uri.path + '/tests'),
-});
+export const loadZigTests = async (workspace: vscode.WorkspaceFolder): Promise<ProjectTests> => {
+    let projectTests: ProjectTests = {
+        testSuiteInfo: {
+            type: 'suite',
+            id: 'root',
+            label: 'Zig Test',
+            children: [],
+        },
+        mapping: {},
+        kinds: [],
+        indices: [],
+        suites: [],
+        filenames: [],
+        tests: [],
+    };
+    await loadTestSuite(projectTests, workspace.uri.path + '/tests', projectTests.testSuiteInfo.children);
+    return projectTests;
+}
 
-export async function runFakeTests(
-    tests: string[],
-    testStatesEmitter: vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>
-): Promise<void> {
-    for (const suiteOrTestId of tests) {
-        const node = findNode(zigTestSuite, suiteOrTestId);
-        if (node) {
-            await runNode(node, testStatesEmitter);
-        }
+type TestStatesEmitter = vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>;
+
+export const runZigTests = async (projectTests: ProjectTests, tests: string[], testStatesEmitter: TestStatesEmitter): Promise<void> => {
+    for (const test of tests) {
+        const index = projectTests.mapping[test];
+        await runTest(projectTests, index, test, testStatesEmitter);
     }
 }
 
-function findNode(searchNode: TestSuiteInfo | TestInfo, id: string): TestSuiteInfo | TestInfo | undefined {
-    if (searchNode.id === id) {
-        return searchNode;
-    } else if (searchNode.type === 'suite') {
-        for (const child of searchNode.children) {
-            const found = findNode(child, id);
-            if (found) return found;
-        }
-    }
-    return undefined;
-}
-
-async function runNode(
-    node: TestSuiteInfo | TestInfo,
-    testStatesEmitter: vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>
-): Promise<void> {
-
-    if (node.type === 'suite') {
-
-        testStatesEmitter.fire(<TestSuiteEvent>{ type: 'suite', suite: node.id, state: 'running' });
-
-        for (const child of node.children) {
-            await runNode(child, testStatesEmitter);
-        }
-
-        testStatesEmitter.fire(<TestSuiteEvent>{ type: 'suite', suite: node.id, state: 'completed' });
-
-    } else { // node.type === 'test'
-
-        testStatesEmitter.fire(<TestEvent>{ type: 'test', test: node.id, state: 'running' });
-
-        testStatesEmitter.fire(<TestEvent>{ type: 'test', test: node.id, state: 'passed' });
-
+const runTest = async (projectTests: ProjectTests, index: number, test: string, testStatesEmitter: TestStatesEmitter,): Promise<void> => {
+    switch (projectTests.kinds[index]) {
+        case Kind.Suite:
+            testStatesEmitter.fire(<TestSuiteEvent>{ type: 'suite', suite: test, state: 'running' });
+            testStatesEmitter.fire(<TestSuiteEvent>{ type: 'suite', suite: test, state: 'completed' });
+            break;
+        case Kind.Test:
+            testStatesEmitter.fire(<TestEvent>{ type: 'test', test: test, state: 'running' });
+            testStatesEmitter.fire(<TestEvent>{ type: 'test', test: test, state: 'passed' });
+            break;
     }
 }
